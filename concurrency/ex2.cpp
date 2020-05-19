48b962b73f3f9060b609ece28be8064f41d4d8b9
orpopoola <olaoluwapopoola@gmail.com>
2020-05-19 20:08:38 +0100

    number of cores

---
+++ concurrency/ex2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <thread>
+
+int main(){
+    unsigned int nCores = std::thread::hardware_concurrency();
+    std::cout<<"This machine supports concurrency with "<<nCores<<" cores available"<<std::endl;
+    return 0;
+}
